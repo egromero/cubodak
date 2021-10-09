dd87de3e3f96bd54d6221dc8ac44cb8f0299e40e
egromero <egromero@uc.cl>
2021-10-09 13:55:12 -0300

    env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], 
-                           ENV['GOOGLE_CLIENT_SECRET'],
+  provider :google_oauth2, ENV['client_id'],
+                           ENV['client_secret'],
                            image_size: 150
 end
 
